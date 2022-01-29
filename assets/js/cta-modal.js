e99c27ab1425618d8ac1fa4c4584ee8e9ed68974
Nathan Smith <nathan@sonspring.com>
2022-01-29 13:38:56 -0600

    Work in progress

--- assets/js/cta-modal.js
+++ assets/js/cta-modal.js
@@ -25,6 +25,14 @@
 
 	const style = `
 		<style>
+			*,
+			*:after,
+			*:before {
+				box-sizing: border-box;
+				margin: 0;
+				padding: 0;
+			}
+
 			.cta-modal__focus-trap {
 				opacity: 0;
 				overflow: hidden;
@@ -39,15 +47,17 @@
 
 			.cta-modal__overlay {
 				background-color: rgba(0, 0, 0, 0.5);
-				display: flex;
 
 				width: 100%;
 				height: 100%;
 
-				z-index: 2;
+				overflow-x: hidden;
+				overflow-y: auto;
+
 				position: fixed;
 				top: 0;
 				left: 0;
+				z-index: 2;
 			}
 
 			.cta-modal__scroll {
@@ -55,12 +65,16 @@
 				align-items: center;
 				justify-content: center;
 
+				padding-top: 30px;
+				padding-bottom: 20px;
+
 				width: 100%;
 				min-height: 100%;
 			}
 
 			.cta-modal {
 				background-color: #fff;
+				border-radius: 4px;
 				padding: 20px;
 
 				width: 500px;
@@ -69,7 +83,7 @@
 				position: relative;
 			}
 
-			.cta-modal > :last-child {
+			.cta-modal > *:last-child {
 				margin-bottom: 0;
 			}
 
@@ -83,16 +97,32 @@
 				border-radius: 50%;
 				box-shadow: 0 0 0 1px #fff;
 
-				font-size: 20px;
-				text-decoration: center;
-
+				cursor: pointer;
+				font-family: 'Arial', sans-serif;
+				font-size: 24px;
 				line-height: 30px;
+				text-align: center;
 				width: 30px;
+
+				transition-duration: 0.25s;
+				transition-property: background-color, box-shadow, color;
 
 				transform: translate(50%, -50%);
 				position: absolute;
 				top: 0;
 				right: 0;
+			}
+
+			.cta-modal__close:hover {
+				color: #000;
+				background-color: #fff;
+				box-shadow: 0 0 0 1px #000;
+			}
+
+			.cta-modal__close:focus-visible {
+				color: #000;
+				background-color: #fff;
+				box-shadow: 0 0 0 1px #000;
 			}
 		</style>
 	`;
@@ -105,7 +135,7 @@
 		<span
 			aria-hidden="true"
 			class="cta-modal__focus-trap"
-			tabindex={0}
+			tabindex="0"
 		></span>
 	`;
 
@@ -114,9 +144,9 @@
 			<button class="cta-modal__toggle" type="button"></button>
 		</p>
 
-		<div class="cta-modal__overlay">
-			${focusTrap}
+		${focusTrap}
 
+		<div class="cta-modal__overlay">
 			<div class="cta-modal__scroll">
 				<div
 					aria-modal="true"
@@ -124,13 +154,18 @@
 					role="dialog"
 					tabindex="-1"
 				>
-					<button class="cta-modal__close" title="Close">&times;</button>
+					<button
+						class="cta-modal__close"
+						title="Close"
+						type="button"
+					>&times;</button>
+
 					<slot />
 				</div>
 			</div>
-
-			${focusTrap}
 		</div>
+
+		${focusTrap}
 	`;
 
 	const markup = [style, modal].join('').trim().replace(/\s+/g, ' ');
@@ -208,7 +243,7 @@
 			this.modalOverlay.removeEventListener(CLICK, this.handleClickOverlay);
 		}
 
-		// Toggle modal.
+		// Helper: toggle modal.
 		toggleModalDisplay() {
 			// Show or hide?
 			this.modalOverlay.style.display = this.isActive ? 'block' : 'none';
@@ -219,15 +254,41 @@
 			}
 		}
 
+		// Helper: detect outside modal.
+		isOutsideModal(element) {
+			// Early exit.
+			if (!this.isActive || !element) {
+				return false;
+			}
+
+			// Get DOM elements.
+			let childList = document.querySelectorAll('cta-modal[active="true"] *');
+			childList = Array.from(childList);
+
+			// Get shadow elements.
+			let shadowList = this.shadowRoot.querySelectorAll('*');
+			shadowList = Array.from(shadowList);
+
+			// Total list.
+			const totalList = childList.concat(shadowList);
+
+			// Get boolean.
+			const bool =
+				!totalList.includes(element) ||
+				element.classList.contains('cta-modal__focus-trap') ||
+				element.classList.contains('cta-modal__toggle');
+
+			// Expose boolean.
+			return bool;
+		}
+
 		// Event: overlay click.
 		handleClickOverlay(event) {
 			// Get layer.
 			const { target } = event;
-			const isOutsideModal = !target.closest('.cta-modal');
 
 			// Outside modal?
-			if (isOutsideModal) {
-				// TODO: DEBUG THIS.
+			if (target.classList.contains('cta-modal__scroll')) {
 				this.handleClickToggle();
 			}
 		}
@@ -236,6 +297,7 @@
 		handleClickToggle() {
 			// Set flag.
 			this.isActive = !this.isActive;
+			this.setAttribute('active', this.isActive);
 
 			// Set display.
 			this.toggleModalDisplay();
@@ -243,17 +305,11 @@
 
 		// Event: focus in document.
 		handleFocusIn() {
-			// Early exit.
-			if (!this.isActive || !document.activeElement) {
-				return;
-			}
-
-			// Get boolean.
-			const isOutsideModal = !document.activeElement.closest('.cta-modal__overlay');
+			// Get active element.
+			const activeElement = this.shadowRoot.activeElement || document.activeElement;
 
 			// Outside modal?
-			if (isOutsideModal) {
-				// TODO: DEBUG THIS.
+			if (this.isOutsideModal(activeElement)) {
 				this.modal.focus();
 			}
 		}
