ca101572d8727793e526d258b18035842b74c4ca
Nathan Smith <nathan@sonspring.com>
2022-02-05 15:38:46 -0600

    Add HTML minifier

---
+++ scripts/postbuild.js
@@ -0,0 +1,34 @@
+// =======
+// Import.
+// =======
+
+const { execSync } = require('child_process');
+
+// =========
+// Commands.
+// =========
+
+let CLI_COMMAND = `
+    html-minifier \
+    --input-dir ./build \
+    --output-dir ./build \
+    --file-ext html \
+    --collapse-whitespace \
+    --decode-entities \
+    --minify-css true \
+    --minify-js true \
+    --remove-comments \
+    --remove-redundant-attributes \
+    --remove-script-type-attributes \
+    --remove-style-link-type-attributes \
+    --use-short-doctype
+`;
+
+CLI_COMMAND = CLI_COMMAND.trim().replace(/\s+/g, ' ');
+
+// =============
+// Run commands.
+// =============
+
+global.console.log(CLI_COMMAND);
+execSync(CLI_COMMAND);
